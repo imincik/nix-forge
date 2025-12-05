8f421240e704759232966b35bf5d4198a5341330
Ivan Mincik <ivan.mincik@gmail.com>
2025-12-05 17:46:57 +0100

    flake: import our flake-module

--- flake.nix
+++ flake.nix
@@ -42,21 +42,25 @@
       ];
 
       imports = [
-        ./forge/modules/forge.nix
-        ./forge/modules/apps.nix
-        ./forge/modules/packages.nix
-        ./forge/packages.nix
+        (import ./forge/flake-module.nix { inherit inputs; })
         ./ui/develop.nix
         ./checks.nix
         ./templates.nix
-
-        ./outputs/all-apps.nix
-        ./outputs/all-packages.nix
       ];
 
       _module.args.rootPath = ./.;
 
       # Export flake module for use in other projects
       flake.flakeModules.default = import ./forge/flake-module.nix { inherit inputs; };
+
+      perSystem = { ... }: {
+        forge = {
+          repositoryUrl = "github:imincik/nix-forge";
+          recipeDirs = {
+            packages = ./outputs/packages;
+            apps = ./outputs/apps;
+          };
+        };
+      };
     };
 }

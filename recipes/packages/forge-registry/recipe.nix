1a490a7321ee3e546efa718bf484892641c6b0b0
Ivan Mincik <ivan.mincik@gmail.com>
2026-01-28 10:45:25 +0100

    recipes(packages/forge-registry): provide gunicorn as passthru output

--- recipes/packages/forge-registry/recipe.nix
+++ recipes/packages/forge-registry/recipe.nix
@@ -26,14 +26,16 @@
     requirements.dependencies = [
       pkgs.python3Packages.flask
     ];
-    requirements.optional-dependencies = {
-      production = [
-        pkgs.python3Packages.gunicorn
-      ];
-    };
     importsCheck = [
       "app"
       "registry"
     ];
+  };
+
+  build.extraDrvAttrs = {
+    passthru = {
+      # Provide gunicorn as a passthru output available for production deployment
+      gunicorn = pkgs.python3Packages.gunicorn;
+    };
   };
 }
